d31c1030761ceb0670b1a33ca1c9e1ed09a31e3d
DanHazard <daniel.love.jr@gmail.com>
2020-05-13 22:52:14 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+require "pry"
+
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(s_name, grade)
+    roster[grade] ||= []
+    roster[grade] << s_name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    roster.each do |grade, s_list|
+      sorted_roster[grade] = s_list.sort
+    end
+    sorted_roster
+  end
+
+
+end
+
+# carmel = School.new("Carmel")
+# binding.pry
